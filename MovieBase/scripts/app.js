7faa05a4be07f24d5b7bc0201d3901a7f1f0c9f1
Kaloyan Gospodinov <kaloyan_gospodinov@yahoo.co.uk>
2013-10-13 11:38:47 -0500

    Changes made by Kaloyan Gospodinov (kaloyan_gospodinov@yahoo.co.uk).

--- MovieBase/scripts/app.js
+++ MovieBase/scripts/app.js
@@ -9,5 +9,14 @@ var app = app || {};
             navigator.notification.vibrate(2000);
             navigator.notification.alert("You should use a wi-fi connection.");
         } 
+        document.addEventListener("offline", onOffline, false);
+        
     });
+    
+    function onOffline() {
+        navigator.notification.vibrate(2000);
+        navigator.notification.alert("You need a connection to use this app.", function() {
+            navigator.app.exitApp();
+        }, "No connection", "Exit");
+    }
 })();
