489863d92eb175eca0f8cbf79718cc7c210fac53
Kaloyan Gospodinov <kaloyan_gospodinov@yahoo.co.uk>
2013-10-13 09:27:46 -0500

    Changes made by Kaloyan Gospodinov (kaloyan_gospodinov@yahoo.co.uk).

--- MovieBase/scripts/app.js
+++ MovieBase/scripts/app.js
@@ -1,8 +1,6 @@
 var app = app || {};
 
 (function() {
-
-    app.servicesBaseUrl = "http://api.rottentomatoes.com/api/public/v1.0/";
     
     document.addEventListener("deviceready", function () {
         var kendoApp = new kendo.mobile.Application(document.body);
